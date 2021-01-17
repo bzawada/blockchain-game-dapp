9db499abdce162ed6076f03930b40d6054dc7fc7
Bartek Zawada <krucjo12@gmail.com>
2021-01-17 14:20:33 +0100

    feat: add MemoryToken

--- src/contracts/MemoryToken.sol
+++ src/contracts/MemoryToken.sol
@@ -3,6 +3,24 @@ pragma solidity ^0.5.0;
 import "./ERC721Full.sol";
 
 contract MemoryToken is ERC721Full {
-  // Code goes here...
+  constructor() ERC721Full("Memory token", "MEMORY") public {
+  }
 
+  function mint(address _to, string memory _tokenURI) public returns(bool) {
+    uint tokenId = totalSupply().add(1);
+    _mint(_to, tokenId);
+    _setTokenURI(tokenId, _tokenURI);
+
+    return true;
+  }
+
+  function burnAll(address owner) public returns(bool) {
+    uint tokenQuantity = totalSupply();
+    uint tokenId;
+    for (tokenId = 1; tokenId <= tokenQuantity; tokenId++) {
+      _burn(owner, tokenId);
+    }
+
+    return true;
+  }
 }
